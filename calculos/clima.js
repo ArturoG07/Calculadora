6ca3dd6ad8457c84c9d92109ecb172af05d673d1
Arturo <arturogregori836@gmail.com>
2026-02-25 13:31:00 +0100

    Implementacion de calculadora de clima

---
+++ calculos/clima.js
@@ -0,0 +1,29 @@
+/*Busca el clima en tiempo real usando la api openweathermap*/
+async function buscarClima() {
+    let pantalla = obtenerPantallaActiva();
+    let zona = document.getElementById("zona").value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${zona}&appid=2c67d8e9416b46d40cbf446c17ba3028&units=metric&lang=es`;
+
+    try {
+        const respuesta = await fetch(url);
+        if (!respuesta.ok) {
+            throw new Error("Ciudad no encontrada");
+        }
+        const datos = await respuesta.json();
+
+        const temperatura = datos.main.temp;
+        const humedad = datos.main.humidity;
+        const viento = (datos.wind.speed * 3.6).toFixed(1);
+        const descripcion = datos.weather[0].description;
+
+        pantalla.innerHTML =
+            `🌡 Temperatura: ${temperatura}°C<hr>` +
+            `💧 Humedad: ${humedad}%<hr>` +
+            `💨 Viento: ${viento} km/h<hr>` +
+            `📝 Descripción: ${descripcion}`;
+        añadirHistorial(`Clima de ${zona}`);
+    } catch (error) {
+        pantalla.textContent = "❌ Error al obtener clima";
+        return null;
+    }
+}
